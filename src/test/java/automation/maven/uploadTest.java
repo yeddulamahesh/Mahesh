4b414968df42ec8b1d5e092a785078b017cd97ad
new <new@Mahesh>
2018-06-26 22:52:43 +0530

    dealing desktop elements with autoit

--- src/test/java/automation/maven/uploadTest.java
+++ src/test/java/automation/maven/uploadTest.java
@@ -2,10 +2,11 @@ package automation.maven;
 
 import org.testng.annotations.Test;
 
-import java.util.Set;
+import java.io.IOException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
@@ -13,9 +14,13 @@ public class uploadTest {
 	WebDriver driver;
 	
   @Test
-  public void uoload() {
+  public void upload() throws Exception {
 	  
 	 driver.findElement(By.xpath("//label[@for='pdf2doc-switch']//span[@class='ui-button-text']")).click();
+	 WebElement focus= driver.findElement(By.xpath("//span[@class='ui-button-text'][contains(text(),'Upload Files')]"));
+	 focus.click();
+	 Runtime.getRuntime().exec("C:\\Users\\new\\Desktop\\sample.exe");
+	 
 	  
   }
   @BeforeTest
